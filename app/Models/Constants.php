3947664ec9e1d8b9d920a7bf38dd47158951f216
odenktools <odenktools86@gmail.com>
2020-06-04 22:53:48 +0700

    Rubah Constants Kategori Donasi

--- app/Models/Constants.php
+++ app/Models/Constants.php
@@ -38,8 +38,8 @@ class Constants
     ];
 
     public const DONATE_CATEGORIES = [
-        array('id' => '00000000-0001-43ad-a18c-000000000001', 'name' => 'Tunai'),
-        array('id' => '00000000-0002-43ad-a18c-000000000002', 'name' => 'Non tunai'),
+        array('id' => '00000000-0001-43ad-a18c-000000000001', 'name' => 'Medis'),
+        array('id' => '00000000-0002-43ad-a18c-000000000002', 'name' => 'Non Medis'),
         array('id' => '00000000-0003-43ad-a18c-000000000003', 'name' => 'Logistik'),
     ];
 }
