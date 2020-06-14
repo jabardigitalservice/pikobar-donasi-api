f6de2f5994d06f705730b903df90fa00a7ce81ce
odenktools <odenktools86@gmail.com>
2020-06-14 20:30:37 +0700

    fixing csv materials seeder

--- app/Http/Controllers/Api/External/LogisticController.php
+++ app/Http/Controllers/Api/External/LogisticController.php
@@ -106,6 +106,7 @@ class LogisticController extends Controller
                         );
                         $num++;
                     }
+                    \App\Libraries\SyncDatabase::writeData($items);
                     return Mapper::array($items, $request->method());
                 } else {
                     return Mapper::object($dataRecords['count'], $request->method());
--- database/seeds/MaterialSeeder.php
+++ database/seeds/MaterialSeeder.php
@@ -12,21 +12,8 @@ class MaterialSeeder extends CsvSeeder
     public function __construct()
     {
         $this->table = 'materials';
-        $this->csv_delimiter = ',';
+        $this->csv_delimiter = '|';
         $this->filename = base_path() . '/database/seeds/csv/materials.csv';
-        $this->mapping = [
-            0 => 'id',
-            1 => 'id_pos',
-            2 => 'matg_id',
-            3 => 'sisa',
-            4 => 'masuk',
-            5 => 'distribusi',
-            6 => 'status',
-            7 => 'status_medis',
-            8 => 'is_show',
-            9 => 'created_at',
-            10 => 'updated_at'
-        ];
     }
 
     public function run()
